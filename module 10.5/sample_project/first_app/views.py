e9916ebc724a36721d7a55135d9f681fc30639f2
Jayef Ahmed <jayefahmed837@gmail.com>
2024-12-26 21:51:23 +0600

    module 10.5

--- module 10.5/sample_project/first_app/views.py
+++ module 10.5/sample_project/first_app/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def home(request):
+    d = {'name' : 'Jayef Ahmed', 'Age': 23, 'friend': [
+    {'name': 'salman', 'age':20},
+    {'name': 'sayem', 'age': 23},
+    {'name': 'ruhan', 'age': 22},
+], 'course': ['python', 'c','java'],'love':"jannat"}
+    return render(request, "index.html",d)
